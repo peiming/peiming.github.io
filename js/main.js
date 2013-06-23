97ad1e03147051533cb553733b059f8ba9db0a16
Rae Liu <happyaray@gmail.com>
2013-06-23 15:57:17 -0700

    slide jquery plugin added

--- js/main.js
+++ js/main.js
@@ -118,11 +118,92 @@ BrowserDetect.init();
 var browser =  BrowserDetect.browser;
 
 $(function () {
+  function projectSlideFn() {
+    var Page = (function() {
+
+      var $navArrows = $( '#nav-arrows' ),
+        $nav = $( '#nav-dots > span' ),
+        slitslider = $( '#slider' ).slitslider( {
+          onBeforeChange : function( slide, pos ) {
+
+            $nav.removeClass( 'nav-dot-current' );
+            $nav.eq( pos ).addClass( 'nav-dot-current' );
+
+          }
+        } ),
+
+        init = function() {
+
+          initEvents();
+
+        },
+        initEvents = function() {
+
+          // add navigation events
+          $navArrows.children( ':last' ).on( 'click', function() {
+
+            slitslider.next();
+            return false;
+
+          } );
+
+          $navArrows.children( ':first' ).on( 'click', function() {
+
+            slitslider.previous();
+            return false;
+
+          } );
+
+          $nav.each( function( i ) {
+
+            $( this ).on( 'click', function( event ) {
+
+              var $dot = $( this );
+
+              if( !slitslider.isActive() ) {
+
+                $nav.removeClass( 'nav-dot-current' );
+                $dot.addClass( 'nav-dot-current' );
+
+              }
+
+              slitslider.jump( i + 1 );
+              return false;
+
+            } );
+
+          } );
+
+        };
+
+        return { init : init };
+
+    })();
+
+    Page.init();
+
+    /**
+     * Notes:
+     *
+     * example how to add items:
+     */
+
+    /*
+
+    var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
+
+    // call the plugin's add method
+    ss.add($items);
+
+    */
+
+  }
+  projectSlideFn();
   function landingPageBehavior() {
     var landingImg = $('.landing').find('img');
     landingImg.on('click', function () {
       landingImg.parent().hide();
-      $('#project-slide').show();
+      $('#project-slide').fadeIn();
     });
   }
 
@@ -215,11 +296,6 @@ $(function () {
   landingPageBehavior();
   preload('img/roku-content-pic', 'ID_Roku_', 17, '#roku');
 
-
-    var projectSlideImg = $('#project-slide').find('img');
-    projectSlideImg.load(function () {
-      console.log('loaded with ', projectSlideImg.height());
-    });
 
   var menu = $('#menu');
   menu.css('margin-right', -menu.find('li').eq(0).outerWidth()*3.2);
